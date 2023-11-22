acbb698cea8ff1e3c71ec742e6ce69fe2587eb98
HuannAnd <huannvicente14@outlook.com>
2023-11-21 22:48:42 -0300

    fixing special characters, missing key props, remove Skills layout

--- src/app/home/layouts/About.tsx
+++ src/app/home/layouts/About.tsx
@@ -28,17 +28,17 @@ export default function About() {
         <article ref={ref} id="about-me" className="grow grid sticky top-0 grid-cols-6 mt-32 gap-4 py-[3vw] align-baseline font-light z-20 col-start-6">
           <Polygon data-scroll data-scroll-speed="-0.2" trigger='#about' className='-z-10' isRegular vertexes={4} radius={200} />
           <TextFadeInOnView className='sticky top-0 col-start-2' delayInSeconds={.2} trigger='#about'>
-            <p className='text-[1rem] mb-4'>I'm Frontend developer, from Brazil</p>
+            <p className='text-[1rem] mb-4'>I&apos;m Frontend developer, from Brazil</p>
             <p className='text-[1rem]'>
-              As a Developer my favorites technologies are Next.js, React.js, TailwindCSS and SCSS. Studying Software Development since 2022, my first contact with Software Development was solving basic problems using C#. Right now I'm studing Next.js, Typescript, Docker, Three.js and C#
+              As a Developer my favorites technologies are Next.js, React.js, TailwindCSS and SCSS. Studying Software Development since 2022, my first contact with Software Development was solving basic problems using C#. Right now I&apos;m studying Next.js, Typescript, Docker, Three.js and C#
             </p>
           </TextFadeInOnView>
           <TextFadeInOnView className='col-start-3' trigger='#about' delayInSeconds={.4}>
             <p className='text-[1rem] mb-4'>
-              My Backends knowledge's are C# (.NET Core), Java Spring Boot and Node.js
+              My Backends knowledge&apos;s are C# (.NET Core), Java Spring Boot and Node.js
             </p>
             <p className='text-[1rem]'>
-              If you haven't seen my projects, please take a look. If you find any bug, or want to contribute, open an issue, PR or contact me
+              If you haven&apos;t seen my projects, please take a look. If you find any bug, or want to contribute, open an issue, PR or contact me
             </p>
           </TextFadeInOnView>
         </article>
