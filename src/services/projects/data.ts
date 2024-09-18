c41f4dc51a922f7d10e5fe603433acc3a7b12e51
HuannAnd <huannvicente14@outlook.com>
2024-09-18 11:15:27 -0300

    add: inserting a new project at data.ts

--- src/services/projects/data.ts
+++ src/services/projects/data.ts
@@ -18,5 +18,14 @@ export default [
     site_url: "https://mercantte-git-master-huannand.vercel.app/",
     design_concept_text: "Creating a plant landing page, being Light, Simple and Smart",
     objetive_text: "The objetive of that website for study, there im use OpenAI API and Trefle API to create all copyright"
+  },
+  {
+    name: "Wilderlee Website", 
+    id: 12312312451, 
+    isInMaintenance: false, 
+    repository_url: "https://github.com/CodeOndeOrganization/wilderlee-website",
+    site_url: "wilderlee-personaltrainer.vercel.app",
+    design_concept_text: "Some text", 
+    objetive_text: "at the moment we don't have any objective."
   }
 ] as IProjectData[]
